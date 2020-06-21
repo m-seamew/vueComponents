4131275f6e93d731d4fc572901fa0bb16acf0ef6
Max Brovchenko <brovchenkomaksym@gmail.com>
2020-06-21 13:01:29 +0300

    component Vue for rendering the list of user profile from github

---
+++ githubCardComponent/app/js/js-site/main-vue.js
@@ -0,0 +1,37 @@
+
+let githubUserCard = {
+  template: '#github-user-card-template',
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      userdata: {}
+    }
+  },
+  created() {
+    axios
+    .get(`https://api.github.com/users/${this.username}`)
+    .then(req => this.userdata = req.data)
+  },
+  computed: {
+    joined: function () {
+      let data = new Date(this.userdata.created_at);
+      let text = 'Joined in ' + data.getFullYear();
+      return text;
+    },
+  }
+}
+
+new Vue({
+    el: '#app',
+    components: {
+      'github-user-card': githubUserCard,
+    },
+    data: {
+      username: ['m-seamew', 'torvalds', 'hydra'],
+    }
+  })
